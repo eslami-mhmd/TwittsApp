e42e0c37702663f71ed135a2776da7e39cd0e50b
Mohammad Eslami <eslami.mhmd@gmail.com>
2022-03-13 03:27:10 +0330

    create project

---
+++ TwittsApp/TwittsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwittsApp
+//
+//  Created by Mohammad Eslami on 3/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
