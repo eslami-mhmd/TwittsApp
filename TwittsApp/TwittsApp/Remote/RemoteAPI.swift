0f334df413a6637cbc61b6b6b280c038e419a1ea
Mohammad Eslami <eslami.mhmd@gmail.com>
2022-03-13 06:30:08 +0330

    Revert "move TwittParams into separate file"
    This reverts commit 439a232f9a2b0ffc090116806480708ec58b3270.

--- TwittsApp/TwittsApp/Remote/RemoteAPI.swift
+++ TwittsApp/TwittsApp/Remote/RemoteAPI.swift
@@ -8,6 +8,41 @@
 import Alamofire
 import Foundation
 
+enum TwittFields: String, Encodable {
+    case createdAt
+    case publicMetrics
+
+    enum CodingKeys: String, CodingKey {
+        case createdAt = "created_at"
+        case publicMetrics = "public_metrics"
+    }
+}
+enum TwittExpansions: String, Encodable {
+    case authorId
+
+    enum CodingKeys: String, CodingKey {
+        case authorId = "author_id"
+    }
+}
+enum UserFields: String, Encodable {
+    case profileImageUrl
+
+    enum CodingKeys: String, CodingKey {
+        case profileImageUrl = "profile_image_url"
+    }
+}
+struct TwittParams: Encodable {
+    let tweetFields: String?
+    let expansions: String?
+    let userFields: String?
+
+    enum CodingKeys: String, CodingKey {
+        case tweetFields = "tweet.fields"
+        case expansions
+        case userFields = "user.fields"
+    }
+}
+
 class RemoteAPI: RemoteAPIProtocol {
   // MARK: - Properties
     @Published private(set) var twitt: TwittResponse?
