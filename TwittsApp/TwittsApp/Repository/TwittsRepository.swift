a78c90cc1851cfe07330d54e17b58e96decdcaf2
Mohammad Eslami <eslami.mhmd@gmail.com>
2022-03-13 04:23:24 +0330

    implement TwittsRepository

---
+++ TwittsApp/TwittsApp/Repository/TwittsRepository.swift
@@ -0,0 +1,44 @@
+//
+//  TwittsRepository.swift
+//  DraftTwitts
+//
+//  Created by Mohammad Eslami on 3/10/22.
+//
+
+import Combine
+import Foundation
+
+class TwittsRepository: TwittsRepositoryProtocol {
+  // MARK: - Properties
+    let remoteAPI: RemoteAPIProtocol
+    @Published private(set) var twitt: TwittResponse?
+    var twittPublished: Published<TwittResponse?> { _twitt }
+    var twittPublisher: Published<TwittResponse?>.Publisher { $twitt }
+    var anyCancel: Set<AnyCancellable> = []
+
+  // MARK: - Methods
+    public init(remoteAPI: RemoteAPIProtocol) {
+        self.remoteAPI = remoteAPI
+    }
+    
+    func fetchTwitts() async throws {
+        remoteAPI.twittPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] twitt in
+                self?.twitt = twitt
+            }.store(in: &anyCancel)
+
+
+        return try await remoteAPI.fetchTwitts()
+    }
+    
+    func updateRule(ruleText: String) async throws {
+        if let id = try await remoteAPI.getRule() {
+            try await remoteAPI.deleteRule(id: id)
+            try await remoteAPI.addRule(ruleText: ruleText)
+        } else {
+            try await remoteAPI.addRule(ruleText: ruleText)
+        }
+    }
+
+}
